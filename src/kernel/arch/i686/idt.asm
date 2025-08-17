269eed070be0b35f12d4e38d7c24dd0b1cac654c
Neel Patel <neelpatel@Neels-MacBook-Air.local>
2025-08-17 08:39:45 -0400

    loaded GDT and IDT for my 32bit kernel

---
+++ src/kernel/arch/i686/idt.asm
@@ -0,0 +1,14 @@
+[bits 32]
+
+global i686_IDT_Load
+i686_IDT_Load:
+    ;make new call frame
+    push ebp
+    mov ebp,esp
+
+    mov eax,[ebp+8]
+    lidt [eax]
+
+    mov esp,ebp
+    pop ebp
+    ret
